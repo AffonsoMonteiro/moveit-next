394a1b2f4273ec1fa424a968d96b7efa91b563b1
AffonsoMonteiro <afonsomonteiro_@hotmail.com>
2021-02-25 23:00:06 -0300

    update page home

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+    return(
+        <div className={styles.profileContainer}>
+            <img src="https://avatars.githubusercontent.com/u/62681086?s=460&u=3a33e1ff7e07f1530e6f57c3f6fcab87e6566650&v=4" alt="Affonso" />
+
+            <div>
+                <strong>Afonso Monteiro</strong>
+                <p>
+                    Level 1
+                </p>
+            </div>
+        </div>
+    )
+}
